6260462e57e4a4fac416ec802565fb919d2b4c98
Edwin Knuth <eknuth@gmail.com>
2017-02-04 18:42:13 -0800

    WIP Remote Test

---
+++ src/remote.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Remote from './remote'
+
+jest.mock('./page', () => 'Page')
+
+describe('Remote', () => {
+  const instance = {
+    "context": undefined,
+    "id": 44000000000,
+    "site": "ward.bay.wiki.org",
+    "slug": "active-journal",
+    "story": undefined,
+    "title": undefined
+  }
+  it('renders correctly', () => {
+    expect(renderRemote({instance})).toMatchSnapshot()
+  })
+})
+
+function renderRemote(props = {}) {
+  return renderer.create(
+    <Remote {...props}></Remote>
+  ).toJSON()
+}
